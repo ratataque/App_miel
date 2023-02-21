45d88e847318693042ea08b5ef86764ab16c2e6c
ratataque <ewanrimmely@outlook.fr>
2023-02-21 16:22:38 +0100

    Login

--- app/src/main/java/com/example/app_miel/Login.java
+++ app/src/main/java/com/example/app_miel/Login.java
@@ -1,10 +1,10 @@
 package com.example.app_miel;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

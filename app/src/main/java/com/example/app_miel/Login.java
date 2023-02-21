ceaff26f13bc4064f85539b027c119a1cd5461e5
estrakio <walterkarl@hotmail.fr>
2023-02-21 17:26:29 +0100

    commentaire

--- app/src/main/java/com/example/app_miel/Login.java
+++ app/src/main/java/com/example/app_miel/Login.java
@@ -3,12 +3,13 @@ package com.example.app_miel;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+// Définiton de la class Login.
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Attache le layout login à la page.
         setContentView(R.layout.login);
     }
 }
--- app/src/main/java/com/example/app_miel/Splash_screen.java
+++ app/src/main/java/com/example/app_miel/Splash_screen.java
@@ -6,23 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+// Définition de la class Splash_screen.
+// Extends AppCompatActivity Permet d'hériter des propriété d'une view
 public class Splash_screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Définition du layout d'affichage.
         setContentView(R.layout.splash_screen);
 
+        // Runnable permet de lancer un sleep de X temps.
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
+                // Définition de la nouvelle page qu'on chargera une fois le sleep terminé.
                 Intent intent = new Intent(getApplicationContext(), Login.class);
 
+                // Lancement de l'activité préparé plu haut.
                 startActivity(intent);
-                //setContentView(R.layout.login);
+
+                // Tue l'activité en cours.
                 finish();
             }
         };
+        // Définition du temps qu'on souhaite attendre ici 3000 milliseconde.
         new Handler().postDelayed(runnable,3000);
 
     }
